234abd0ccd524d8ea3b472ebc07c09ca35816b7d
Rabiul150559 <rbiulislam10571@gmail.com>
2024-06-09 12:32:34 +0600

    practice

---
+++ NetBeansProjects/DbCrud/src/dbcrudmain/DBCrudMain.java
@@ -0,0 +1,19 @@
+package dbcrudmain;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import util.DbUtil;
+
+public class DBCrudMain {
+
+    static PreparedStatement ps;
+    static ResultSet rs;
+    static DbUtil db;
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+
+}
